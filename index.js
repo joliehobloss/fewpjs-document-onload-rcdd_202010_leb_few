9c802473dc03b7edaeeab858771beff2ae08e114
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-18 08:15:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("text")
+  document.getElementById("text").innerHTML="";
   //console.log("The DOM has loaded");
 });
 
